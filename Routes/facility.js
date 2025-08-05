e31f709ed14e8689a8c7dd53827f9368111ead96
ankitmohapatra966 <ankitmohapatra966@gmail.com>
2025-08-05 08:43:03 +0530

    change in API End Point

--- Routes/facility.js
+++ Routes/facility.js
@@ -3,10 +3,10 @@ const router= express.Router();
 const Authentication = require('../Authentication/auth')
 const FacilityController = require('../Controllers/facility')
 
-router.post('/add',Authentication.adminFacultyAuth,FacilityController.addFacility);
-router.put('/update/:id',Authentication.adminFacultyAuth,FacilityController.updateFacility)
-router.get('/get',FacilityController.getAllFacility);
-router.delete('/delete/:id',Authentication.adminFacultyAuth,FacilityController.deleteFacility)
+router.post('/api/add',Authentication.adminFacultyAuth,FacilityController.addFacility);
+router.put('/api/update/:id',Authentication.adminFacultyAuth,FacilityController.updateFacility)
+router.get('/api/get',FacilityController.getAllFacility);
+router.delete('/api/delete/:id',Authentication.adminFacultyAuth,FacilityController.deleteFacility)
 
 
 
